e39384042cb6c0ff79b9374f992c98523e9f99b5
hrb-20222202 <129731654+hrb-20222202@users.noreply.github.com>
2024-03-11 21:57:35 +0800

    Add files via upload

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,10 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import loginView from '@/views/loginView.vue'
 import first from '@/views/first.vue'
 import register from '@/views/register.vue'
-import adminMain from '@/views/admin/main.vue'
+
 import userMain from '@/views/user/main.vue'
+import sharing_resourceVue from '@/views/user/sharing_resource.vue'
+
+import adminMain from '@/views/admin/main.vue'
 import ManageUsers from '@/views/admin/ManageUsers.vue'
 import ResourcesSharing from '@/views/admin/ResourceSharing.vue'
+import CourseCustomization from '@/views/admin/CourseCustomization.vue'
+import DigitalAssistant from '@/views/admin/DigitalAssistant.vue'
+import FourC_Resource from '@/views/admin/4C_Resource.vue'
+
+import main from '@/views/user/main.vue'
 const routes = [
   {
     path: '/login',
@@ -23,16 +31,40 @@ const routes = [
     component:adminMain,
     children:[
       {
+        //用户管理
         path:'/adminMain/users',
         name:'ManageUsers',
         component:ManageUsers
       },
       {
+        //共享资源
         path:'/adminMain/sharing',
         name:'ResourceSharing',
         component:ResourcesSharing
+      },
+      {
+        //定制资源
+        path:'/adminMain/customize',
+        name:'CourseCustomize',
+        component:CourseCustomization
+      },
+      {
+        path:'/adminMain/fourC',
+        name:'4C',
+        component:FourC_Resource
+      },
+      {
+        //数字助手
+        path:'/adminMain/digital',
+        name:'DigitalAssistant',
+        component:DigitalAssistant
       }
     ]
+  },
+  {
+    path:'/',
+    name:'userMain',
+    component:main,
   },
   {
     path:'/register',
@@ -43,6 +75,13 @@ const routes = [
     path:'/userMain',
     name:'userMain',
     component:userMain,
+    children:[
+      {
+        path:'/userMain/sharing',
+        name:'sharing',
+        component:sharing_resourceVue
+      }
+    ]
   }
 
 ]
